571c568033642daa43d7deeb61f781b28867783e
Adam A. Pratt <adampratt95@gmail.com>
2025-05-15 00:10:48 -0500

    Update conda_prov.sh

--- conda_prov.sh
+++ conda_prov.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# 
-cd /workspace/
+
 # Cause the script to exit on failure.
 set -eo pipefail
 
@@ -9,7 +8,7 @@ set -eo pipefail
 . /venv/main/bin/activate
 
 # Download some useful files
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-sudo bash ./Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/anaconda3
+wget -P ~/ https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+sudo bash ~/Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/anaconda3
 sudo source ~/anaconda3/bin/activate
 conda init --all
