7cd2537dbe8b6e7432ae18dd7a22d8f6b439830d
Adam A. Pratt <adampratt95@gmail.com>
2025-05-14 23:16:02 -0500

    Create conda_prov.sh

---
+++ conda_prov.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 
+cd /workspace/
+# Cause the script to exit on failure.
+set -eo pipefail
+
+# Activate the main virtual environment
+. /venv/main/bin/activate
+
+# Download some useful files
+wget -P "${WORKSPACE}/" https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+~/Miniconda3-latest-Linux-x86_64.sh
